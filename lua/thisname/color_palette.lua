2581222f3d667c8f03814daf37a548489c5c8217
punktigel <punktigel@mailbox.org>
2023-10-22 16:33:07 +0200

    Remove unused colors - color palette

--- lua/thisname/color_palette.lua
+++ lua/thisname/color_palette.lua
@@ -23,31 +23,22 @@ local variants = {
         gray_dark = '#757575',
 
         green_blue = '#3adeb8',
-        green_blue_light = '#42ffd4',
-        -- green_blue_dark = '#32bf9f',
-        -- g_green_blue= '#3ade8f',
 
         purple = '#eb34d5',
         purple_dark = '#b327a2',
 
         blue_light = '#b3ceff',
         blue = '#3d71cc',
-        blue_dark = '#2e5499',
-
-        yellow = '#e8eb34',
-        yellow_dark = '#b1b327',
 
         red = '#eb4667',
-        red_dark = '#a8324a',
 
         orange = '#ebb146',
-        orange_dark = '#665530', -- '#cc933d',
+        orange_dark = '#bf9d5a',
 
         d_red = '#eb4667',
-        d_orange = '#998148', --'#ebc66f', -- '#e68517',
-        d_green = '#489968', --'#6feb90', -- '#34eb4c',
+        d_orange = '#ebc66f',
+        d_green = '#489968',
 
-        -- col1 = '#a83268',
     },
 
     dark_blue = {
